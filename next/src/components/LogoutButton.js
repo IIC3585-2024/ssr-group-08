feba8eae949ba1479d0e2d8e836e325fbbc94b4c
Vicente Cruz <vcrb@uc.cl>
2024-06-25 14:07:10 -0400

    feat - logout and navbar for app implemented

---
+++ next/src/components/LogoutButton.js
@@ -0,0 +1,24 @@
+"use client";
+
+import { logout } from "@/app/lib/actions";
+import { useRouter } from "next/navigation";
+
+export default function LogoutButton() {
+  const router = useRouter();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    logout();
+    router.push("/auth/login");
+  };
+
+  return (
+    <button
+      className="text-white ml-4"
+      href="/auth/login"
+      onClick={handleClick}
+    >
+      Logout
+    </button>
+  );
+}
